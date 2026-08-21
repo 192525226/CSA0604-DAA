29125d05711684c6b2de4ca94956d839481e011e
Baba Satya Kumar k <192525226.simats@saveetha.com>
2026-08-21 14:42:23 +0530

    Add files via upload

---
+++ lab experiments/wordbreak.py
@@ -0,0 +1,17 @@
+s = input("Enter string: ")
+words = input("Enter dictionary words: ").split()
+
+n = len(s)
+dp = [False] * (n + 1)
+dp[0] = True
+
+for i in range(1, n + 1):
+    for j in range(i):
+        if dp[j] and s[j:i] in words:
+            dp[i] = True
+            break
+
+if dp[n]:
+    print("Word can be segmented")
+else:
+    print("Word cannot be segmented")
